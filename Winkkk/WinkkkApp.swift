5193b600fa6e1f5e27e06c6a4aae053a2f8d57ea
jiayingdeng <280004236@qq.com>
2025-09-15 15:42:36 +0800

    Initial Commit

---
+++ Winkkk/WinkkkApp.swift
@@ -0,0 +1,17 @@
+//
+//  WinkkkApp.swift
+//  Winkkk
+//
+//  Created by tw1234 on 2025/9/15.
+//
+
+import SwiftUI
+
+@main
+struct WinkkkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WinkkkTests/WinkkkTests.swift
@@ -0,0 +1,17 @@
+//
+//  WinkkkTests.swift
+//  WinkkkTests
+//
+//  Created by tw1234 on 2025/9/15.
+//
+
+import Testing
+@testable import Winkkk
+
+struct WinkkkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
